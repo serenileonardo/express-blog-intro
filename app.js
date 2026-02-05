37e06d9e88389cf7fee17a7242b81202ce2212f5
serenileonardo <serenileonardo.web@gmail.com>
2026-02-05 17:35:24 +0100

    add img and array

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
+// attivazione della cartella public per uso file statici
+app.use(express.static('public'));
+
+// rotta index APP
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send("<h1>Server del mio blog</h1>")
 })
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+
+app.get('/posts', (req, res) => {
+    const posts = [
+        {
+            titolo: "Post 1",
+            contenuto: "Contenuto post 1",
+            immagine: "/images/post1.jpg",
+            tags: ["blog"]
+        },
+        {
+            titolo: "Post 2",
+            contenuto: "Contenuto post 2",
+            immagine: "/images/post2.jpg",
+            tags: ["express"]
+        },
+        {
+            titolo: "Post 3",
+            contenuto: "Contenuto post 3",
+            immagine: "/images/post3.jpg",
+            tags: ["node"]
+        },
+        {
+            titolo: "Post 4",
+            contenuto: "Contenuto post 4",
+            immagine: "/images/post4.jpg",
+            tags: ["api"]
+        },
+        {
+            titolo: "Post 5",
+            contenuto: "Contenuto post 5",
+            immagine: "/images/post5.jpg",
+            tags: ["server"]
+        }
+    ];
+
+    res.json(posts);
+
 })
